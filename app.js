0fd487556885f805bcfd10cffdbb5504e4b121a0
Chu Shao <cshao@churenshao.com>
2014-04-19 14:02:01 -0700

    Fbgraph set up

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
 var app = express();
+//load environment variables
+var dotenv = require('dotenv');
+dotenv.load();
 
-//route files to load
-var index = require('./routes/index');
+//fbgraph
+var graph = require('fbgraph');
 
-//database setup - uncomment to set up your database
-//var mongoose = require('mongoose');
-//mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/DATABASE1);
+//twitter
+var passport = require('passport');
+var util = rquire('util');
+var TwitterStrategy = require('passport-twitter').Strategy;
 
 //Configures the Template engine
 app.engine('handlebars', handlebars());
@@ -20,7 +24,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.bodyParser());
 
 //routes
-app.get('/', index.view);
+app.get('/', function(req,res) { 
+	res.render("index");
+});
+
+//fbgraph authentication
+app.get('/auth/facebook', function(req, res) {
+	if (!req.query.code) {
+		var authUrl = graph.getOauthUrl({
+			'client_id': process.env.facebook_client_id,
+			'redirect_uri': 'http://localhost:3000/auth/facebook',
+			'scope': 'user_about_me'//you want to update scope to what you want in your app
+		});
+
+		if (!req.query.error) {
+			res.redirect(authUrl);
+		} else {
+			res.send('access denied');
+		}
+		return;
+	}
+	graph.authorize({
+		'client_id': process.env.facebook_client_id,
+		'redirect_uri': 'http://localhost:3000/auth/facebook',
+		'client_secret': process.env.facebook_client_secret,
+		'code': req.query.code
+	}, function( err, facebookRes) {
+		res.redirect('/UserHasLoggedIn');
+	});
+});
+
+app.get('/UserHasLoggedIn', function(req, res) {
+	graph.get('me', function(err, response) {
+		data = { facebookData: response};
+		res.render('facebook', data);
+	});
+});
+
+
+
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
 http.createServer(app).listen(app.get('port'), function(){
