3e26c933aa16d67fa7ba80e7b403a64b31a8a7cb
Chu Shao <cshao@churenshao.com>
2014-04-19 15:22:42 -0700

    Set up twitter auth

--- app.js
+++ app.js
@@ -11,10 +11,58 @@ dotenv.load();
 //fbgraph
 var graph = require('fbgraph');
 
-//twitter
+//twit
+var twit = require('twit');
+
+//twitter oauth
 var passport = require('passport');
-var util = rquire('util');
-var TwitterStrategy = require('passport-twitter').Strategy;
+var util = require('util');
+var passportTwitterStrategy = require('passport-twitter').Strategy;
+
+//have two blank strings for access token and access secret
+var accessToken = "";
+var accessSecret = "";
+var twitterOauth = {
+	consumer_key: process.env.twitter_client_id,
+	consumer_secret: process.env.twitter_client_secret,
+	access_token: accessToken,
+	access_token_secret: accessSecret
+};
+
+//Set up passport session set up.
+//This allows persistant login sessions so the user doesn't need to keep logging in everytime
+//for their access token
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+
+// Simple route middleware to ensure user is authenticated.
+function ensureAuthenticated(req, res, next) {
+	if (req.isAuthenticated()) { return next(); }
+	res.redirect('/');
+}
+
+
+//Use TwitterStrategy with passport
+passport.use(new passportTwitterStrategy({
+	consumerKey: process.env.twitter_client_id,
+	consumerSecret: process.env.twitter_client_secret,
+	callbackURL: "http://localhost:3000/auth/twitter/callback"
+}, function (token, tokenSecret, profile, done) {
+	//setting up access token
+	accessToken = token;
+	accessSecret = tokenSecret;
+	twitterOauth.access_token = token;
+	twitterOauth.access_token_secret = tokenSecret;
+	//Continuing on
+	process.nextTick(function() {
+		return done(null, profile);
+	});
+}));
 
 //Configures the Template engine
 app.engine('handlebars', handlebars());
@@ -22,6 +70,14 @@ app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.bodyParser());
+//more setting up configuration for express
+//Allows cookie access and interaction
+app.use(express.cookieParser() );
+app.use(express.session({ secret: 'nyan cat'}));
+//Intialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
 
 //routes
 app.get('/', function(req,res) { 
@@ -56,12 +112,36 @@ app.get('/auth/facebook', function(req, res) {
 
 app.get('/UserHasLoggedIn', function(req, res) {
 	graph.get('me', function(err, response) {
+		console.log(err); //if there is an error this will return a value
 		data = { facebookData: response};
 		res.render('facebook', data);
 	});
 });
 
 
+//twitter authentication Oauth setup
+//this will set up twitter oauth for any user not just one
+app.get('/auth/twitter', passport.authenticate('twitter'), function(req, res) {
+	//nothing here because callback redirects to /auth/twitter/callback
+});
+
+//callback. authenticates again and then goes to twitter
+app.get('/auth/twitter/callback', 
+	passport.authenticate('twitter', { failureRedirect: '/' }),
+	function(req, res) {
+		res.redirect('/twitter');
+	});
+
+
+app.get('/twitter', ensureAuthenticated, function(req, res) {
+	//I can use twitterOauth as previously it's an array set up with the correcet information
+	var T = new twit(twitterOauth); 
+	T.get('/friends/list', function (err, reply) {
+		console.log(err); //If there is an error this will return a value
+		data = { twitterData: reply };
+		res.render('twitter', data);
+	});
+});
 
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
